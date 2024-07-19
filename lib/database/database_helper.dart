5674a8fe64fd25de6a36e6936d24f012c3b88a80
Elias Jose Mari�ez Perez <20221073@itla.edu.do>
2024-07-19 02:24:03 -0400

    Modelo y Base de datos

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,21 +1,66 @@
-
-
+// Elias Mariñez, 2022-1073
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:vigilapp_20221073/models/incident_model.dart';
 
 class DatabaseHelper{
+  // Definicion del Singleton DatabaseHelper
   static final DatabaseHelper _instance = DatabaseHelper._internal();
   factory DatabaseHelper() => _instance;
   static Database? _database;
 
   DatabaseHelper._internal();
 
+  // Retorno de la base de datos, en caso de ser nula, la inicializa.
   Future<Database> get database async{
     if(_database != null) return _database!;
     _database = await _initDatabase();
     return _database!;
   }
 
+  // Inicializacion de la base datos, definicion de ruta, version, y creacion de la misma.
   Future<Database> _initDatabase() async{
-    return null;
+    final path = join(await getDatabasesPath(), 'surveillance.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+      );
+  }
+
+  // Creacion de la tabla incidents
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE incidents (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        date TEXT,
+        description TEXT,
+        photo TEXT,
+        audio TEXT
+      )
+    ''');
+  }
+
+  Future<int> insertIncident(Incident incident) async {
+    Database db = await database;
+    return await db.insert('incidents', incident.toMap());
+  }
+
+  Future<List<Incident>> getIncidents() async {
+    Database db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      'incidents', 
+      orderBy: 'date DESC'
+    );
+
+    return List.generate(maps.length, (i) {
+      return Incident.fromMap(maps[i]);
+    });
+  }
+
+  Future<int> deleteAllIncidents() async {
+    Database db = await database;
+    return await db.delete('incidents');
   }
 }
---
+++ lib/models/incident_model.dart
@@ -0,0 +1,39 @@
+class Incident {
+  int? id;
+  String title;
+  DateTime date;
+  String description;
+  String? photo;
+  String? audio;
+
+  Incident({ 
+    this.id, 
+    required this.title,
+    required this.date,
+    required this.description,
+    this.photo,
+    this.audio 
+    });
+
+  Map<String, dynamic> toMap(){
+    return{
+      'id': id,
+      'title': title,
+      'date': date,
+      'description': description,
+      'photo': photo,
+      'audio': audio
+    };
+  }
+
+  factory Incident.fromMap(Map<String, dynamic> map){
+    return Incident(
+      id: map['id'],
+      title: map['title'], 
+      date: map['date'], 
+      description: map['description'],
+      photo: map['photo'],
+      audio: map['audio']
+      );
+  }
+}
